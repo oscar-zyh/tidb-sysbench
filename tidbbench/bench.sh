eb84dcda151debf97f8f9ee4dfbae1e239676546
张亿皓 <yihaozhang@xiaohongshu.com>
2018-09-14 15:57:17 +0800

    add tidb bench mark scripts

---
+++ tidbbench/bench.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# load configurations
+. ./conf.sh
+
+# set global parameters
+if [ $# -ne 2 ]; then
+  echo "usage: $0 <benchtype(0:readonly,1:writeonly,2:readwrite)> <threads>"
+  exit -1
+fi
+btype=$1
+threads=$2
+logfile=$(readlink -f ./bench-B${btype}-T${threads}-$(date '+%Y%m%d-%H%M%S').log)
+lua_dir=../src/lua
+
+# redirect output to the log file
+exec > $logfile
+exec 2>&1
+
+# bench
+cd ${lua_dir}
+
+echo "======================== Start Sysbench ========================"
+echo "bench log append to: ${logfile}"
+
+if [ ${btype} -eq 0 ]; then
+  echo "-------------------- bench read-only --------------------"
+  echo "-------------------- threads: ${threads} --------------------"
+  ../sysbench oltp_read_only.lua \
+    --db-driver=mysql \
+    --mysql-host=${host} \
+    --mysql-port=${port} \
+    --mysql-db=${dbname} \
+    --mysql-user=${user} \
+    --mysql-password=${password} \
+    --table_size=${tsize} \
+    --tables=${tcount} \
+    --threads=${threads} \
+    --events=${maxevents} \
+    --report-interval=${interval} \
+    --time=${maxtime} \
+    run
+elif [ ${btype} -eq 1 ]; then
+  echo "-------------------- bench write-only --------------------"
+  echo "-------------------- threads: ${threads} --------------------"
+  ../sysbench oltp_write_only.lua \
+    --db-driver=mysql \
+    --mysql-host=${host} \
+    --mysql-port=${port} \
+    --mysql-db=${dbname} \
+    --mysql-user=${user} \
+    --mysql-password=${password} \
+    --table_size=${tsize} \
+    --tables=${tcount} \
+    --threads=${threads} \
+    --events=${maxevents} \
+    --report-interval=${interval} \
+    --time=${maxtime} \
+    run
+elif [ ${btype} -eq 2 ]; then
+  echo "-------------------- bench read-write --------------------"
+  echo "-------------------- threads: ${threads} --------------------"
+  ../sysbench oltp_read_write.lua \
+    --db-driver=mysql \
+    --mysql-host=${host} \
+    --mysql-port=${port} \
+    --mysql-db=${dbname} \
+    --mysql-user=${user} \
+    --mysql-password=${password} \
+    --table_size=${tsize} \
+    --tables=${tcount} \
+    --threads=${threads} \
+    --events=${maxevents} \
+    --report-interval=${interval} \
+    --time=${maxtime} \
+    run
+fi
+
+
---
+++ tidbbench/conf.sh
@@ -0,0 +1,17 @@
+
+host=127.0.0.1
+port=4000
+user=root
+password=''
+tcount=1
+tsize=20000000
+dbname=sbtest
+
+# report interval
+interval=10
+
+# max time (s)
+maxtime=300
+
+# max events
+maxevents=500000
